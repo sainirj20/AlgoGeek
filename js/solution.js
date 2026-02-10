9d0d90148130c8dbbe3f8244a5eec59a5c07d250
sainirj20 <saini.rj20@gmail.com>
2026-02-11 01:09:15 +0530

    more pages

--- js/solution.js
+++ js/solution.js
@@ -18,8 +18,10 @@ const renderer = {
                     if (j == 0 || j == temp.length-1 ) continue;
                     newHtml += "<span style='line-height:0.8; display:block;'>\u00A0</span>";
                 } else {
-                    newHtml += xxx + "<br />";
-                }                
+                    newHtml += (xxx.endsWith('ul>') || xxx.endsWith('li>'))
+                            ? xxx 
+                            : xxx + "<br />";
+                }
             }
             htmls[i].innerHTML = newHtml;
         }
